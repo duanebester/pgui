34ffbb1ae078d570cf6286201e30eb0027bf5641
Duane Bester <bester.duane@gmail.com>
2025-07-30 13:01:56 -0500

    Add health_checker module to services

--- src/services/mod.rs
+++ src/services/mod.rs
@@ -1,7 +1,9 @@
 pub mod database;
 pub mod sql_analyzer;
 pub mod connection_monitor;
+pub mod health_checker;
 
 pub use database::*;
 pub use sql_analyzer::*;
 pub use connection_monitor::*;
+pub use health_checker::*;

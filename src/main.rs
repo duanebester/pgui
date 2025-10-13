c8b0ea62d926a186269d3d6625b25ba0ed387334
Duane Bester <bester.duane@gmail.com>
2025-10-13 08:51:28 -0500

    use crates.io deps and remove broken highlighter init call

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,7 @@ use themes::*;
 use window::*;
 
 use gpui::*;
-use gpui_component::{ActiveTheme as _, highlighter, theme};
+use gpui_component::{ActiveTheme as _, Root, init, theme};
 use workspace::Workspace;
 
 fn main() {
@@ -18,13 +18,12 @@ fn main() {
     application.run(|cx: &mut App| {
         let window_options = get_window_options(cx);
         cx.open_window(window_options, |win, cx| {
-            gpui_component::init(cx);
-            highlighter::init(cx);
+            init(cx);
             theme::init(cx);
             change_color_mode(cx.theme().mode, win, cx);
 
             let workspace_view = Workspace::view(win, cx);
-            cx.new(|cx| gpui_component::Root::new(workspace_view.into(), win, cx))
+            cx.new(|cx| Root::new(workspace_view.into(), win, cx))
         })
         .unwrap();
     });
